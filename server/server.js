46ae54d92cda5b8aa0034b611bf067a5aac98969
Spencer Faust <faus0059@umn.edu>
2019-04-02 15:39:32 -0500

    Initial setup.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+http.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
